3f7139c371a51bc7da88c424d9e2ae6207d0f6d5
fabianofrank <fabianofrank@protonmail.com>
2022-06-27 14:55:47 -0300

    Install Rubocop

--- lib/item.rb
+++ lib/item.rb
@@ -1,24 +1,25 @@
 class Item
-    attr_reader :genre, :author, :source, :label, :publish_date
-    attr_accessor :archived
-    def initialize(genre, author, source, label, publish_date, archived: true)
-        @genre = genre
-        @author = author
-        @source = source
-        @label = label
-        @publish_date = publish_date
-        @archived = archived
-    end
+  attr_reader :genre, :author, :source, :label, :publish_date
+  attr_accessor :archived
 
-    def can_be_archived?
-        publish_date > 10.years.ago
-    end
+  def initialize(genre, author, source, label, publish_date)
+    @id = Random.rand(1..1000)
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
+    @archived = archived
+  end
 
-    def move_to_archive
-        if(can_be_archived?)
-            @archived = true
-        end
-    end
+  def can_be_archived?
+    ten = 10
+    publish_date > ten.to_s.years.ago
+  end
 
-    private :can_be_archived?
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
 end
+
+puts Item.new('scifi', 'isaac azimov', 'something', 'foundation', '2020/12/31').can_be_archived?
