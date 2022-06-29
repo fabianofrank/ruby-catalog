a6b224c2ef8b0206a8bbc23cd6146b08e45475e5
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-06-29 11:04:34 -0400

    Create book & label classes

---
+++ lib/book.rb
@@ -0,0 +1,26 @@
+require_relative 'item'
+require_relative 'label'
+
+class Book < Item
+  attr_accessor :cover_state, :publisher
+  
+  def initialize(author, label, publish_date, cover_state, publisher: false)
+    super(genre, author, source, label, publish_date)
+    @cover_state = cover_state
+    @publisher = publisher
+    @label = label
+  end
+
+  def can_be_archived?
+    if can_be_archived? || @cover_state == "bad"
+      true
+    else
+      false
+    end
+  end
+
+  def label=(label)
+    @label = label
+    label.items << self unless label.items.include?(self)
+  end
+end
