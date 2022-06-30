311a3a886eacedef05cc1667a84692c7f6a80415
fasaldana <fernando_s_98@hotmail.com>
2022-06-30 02:04:57 -0500

    fix linters

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -4,28 +4,28 @@ require_relative '../lib/game'
 
 ## testing for author's methods
 RSpec.describe Author do
-    let(:author) { Author.new('John', 'Doe') }
-    let(:game) { Game.new('Action', 'Nintendo', 'Nintendo', '2022-01-01', true) }
+  let(:author) { Author.new('John', 'Doe') }
+  let(:game) { Game.new('Action', 'Nintendo', 'Nintendo', '2022-01-01', true) }
 
-    context '#initialize' do
-        it 'should initialize with a first name' do
-            expect(author.first_name).to eq('John')
-        end
-        it 'should initialize with a last name' do
-            expect(author.last_name).to eq('Doe')
-        end
-        it 'should initialize with an id' do
-            expect(author.id).to be_a(Integer)
-        end
-        it 'should initialize with an empty array of items' do
-            expect(author.items).to eq([])
-        end
+  context '#initialize' do
+    it 'should initialize with a first name' do
+      expect(author.first_name).to eq('John')
     end
+    it 'should initialize with a last name' do
+      expect(author.last_name).to eq('Doe')
+    end
+    it 'should initialize with an id' do
+      expect(author.id).to be_a(Integer)
+    end
+    it 'should initialize with an empty array of items' do
+      expect(author.items).to eq([])
+    end
+  end
 
-    context '#add_item' do
-        it 'should add an item to the author' do
-            author.add_item(game)
-            expect(author.items.length).to eq 1
-        end
+  context '#add_item' do
+    it 'should add an item to the author' do
+      author.add_item(game)
+      expect(author.items.length).to eq 1
     end
+  end
 end
