d6d79226841b024fc30be5d8184edb76f3f6003d
fabianofrank <fabianofrank@protonmail.com>
2022-07-01 16:49:49 -0300

    Create schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,14 +1,80 @@
-CREATE TABLE game (
-    id serial NOT NULL,
-    published_date DATE,
-    last_played_at DATE,
-    is_multiplayer BOOLEAN,
-    label varhar(50),
-    author_id INTEGER,
+CREATE TABLE genre (
+    id SERIAL PRIMARY KEY,
+    name varhar(50) NOT NULL,
+    items text[] NOT NULL
 );
 
 CREATE TABLE author (
-    id serial NOT NULL,
-    first_name varhar(50),
-    last_name varhar(50),
+    id SERIAL PRIMARY KEY,
+    first_name varhar(50) NOT NULL,
+    last_name varhar(50) NOT NULL,
+    items text[] NOT NULL
+);
+
+CREATE TABLE source (
+    id SERIAL PRIMARY KEY,
+    name varhar(50) NOT NULL,
+    items text[]
+);
+
+CREATE TABLE label (
+    id SERIAL PRIMARY KEY,
+    title varhar(50) NOT NULL,
+    color varhar(50) NOT NULL,
+    items text[] NOT NULL
+);
+
+CREATE TABLE item (
+    id SERIAL PRIMARY KEY,
+    publish_date DATE NOT NULL,
+    archived BOOLEAN NOT NULL,
+    genre_id varchar(50) NOT NULL,
+    author_id varchar(50) NOT NULL,
+    source_id varchar(50) NOT NULL,
+    label_id varchar(50) NOT NULL,
+    
+    genre_id int REFERENCES genre(id)
+    author_id int REFERENCES author(id)
+    source_id int REFERENCES source(id)
+    label_id int REFERENCES label(id)
+)
+
+CREATE TABLE game (
+    id SERIAL PRIMARY KEY,
+    publish_date DATE NOT NULL,
+    last_played_at DATE NOT NULL,
+    is_multiplayer BOOLEAN NOT NULL,
+    label varchar(50) NOT NULL,
+    author_id INTEGER NOT NULL,
+
+    author_id int REFERENCES item(id)
+);
+
+CREATE TABLE movie (
+    id SERIAL PRIMARY KEY,
+    publish_date DATE NOT NULL,
+    label varhar(50) NOT NULL,
+    silent BOOLEAN NOT NULL,
+    source_id INTEGER NOT NULL,
+
+    source_id int REFERENCES item(id)
+);
+
+CREATE TABLE music (
+    id SERIAL PRIMARY KEY,
+    publish_date DATE NOT NULL,
+    on_spotify BOOLEAN NOT NULL,
+    genre_id INTEGER NOT NULL,
+
+    genre_id int REFERENCES item(id)
+);
+
+CREATE TABLE book (
+    id SERIAL PRIMARY KEY,
+    publish_date DATE,
+    cover_state varhar(50) NOT NULL,
+    publisher BOOLEAN NOT NULL,
+    label_id INTEGER NOT NULL,
+
+    label_id int REFERENCES item(id)
 );
